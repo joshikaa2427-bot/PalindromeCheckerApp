7b973b2f7ca22eeddef086148fb8b93d79f8f52b
Joshi <joshika.a2427@gmail.com>
2026-03-10 15:29:31 +0530

    Joshika Added UC4 Character Array Based Palindrome Check

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,21 @@
-// version 2.0
-// author Abc
-// UseCase 2: Hardcoded Palindrome Check
 
-public class PalindromeCheckerApp {
-    public static void main(String[] args){
+import java.util.Scanner;
+public class UseCase4PalindromeCheckerApp {
+    public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
 
-        // UC2 Logic: Hardcoded palindrome check
-        String word = "madam";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word or phrase: ");
+        String input = sc.nextLine();
+
+        char[] chars = input.toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
         boolean isPalindrome = true;
 
-        int left = 0, right = word.length() - 1;
         while (left < right) {
-            if (word.charAt(left) != word.charAt(right)) {
+            if (chars[left] != chars[right]) {
                 isPalindrome = false;
                 break;
             }
@@ -21,9 +24,11 @@ public class PalindromeCheckerApp {
         }
 
         if (isPalindrome) {
-            System.out.println("Result: \"" + word + "\" is a palindrome ✅");
+            System.out.println("Result: \"" + input + "\" is a palindrome ✅");
         } else {
-            System.out.println("Result: \"" + word + "\" is NOT a palindrome ❌");
+            System.out.println("Result: \"" + input + "\" is NOT a palindrome ❌");
         }
+
+        sc.close();
     }
 }
